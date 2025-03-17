152f810893bfbe00c67f6a29534bf2b9b5f27550
Nikhilg0208 <nikhilg1098@gmail.com>
2025-03-17 21:05:42 +0530

    dashboard and transaction page added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,37 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Loader from "./components/common/Loader";
 import Home from "./components/Home";
-// const Login = lazy(() => import("./pages/Login"));
+import { SignedIn } from "@clerk/clerk-react";
+
+const Transaction = lazy(() => import("./pages/Transaction"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 function App() {
   return (
-    <>
-      <Router>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/login" element={<h1>Login</h1>} /> */}
-          </Routes>
-        </Suspense>
-      </Router>
-    </>
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/transaction/create"
+            element={
+              <SignedIn>
+                <Transaction />
+              </SignedIn>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
