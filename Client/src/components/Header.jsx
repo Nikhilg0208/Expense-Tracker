0a249dd42f954a77dc8fa99d18e1c483457eec1e
Nikhilg0208 <nikhilg1098@gmail.com>
2025-03-17 20:34:02 +0530

    changes added

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { GrTransaction } from "react-icons/gr";
+
 const Header = () => {
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border border-gray-200">
@@ -15,48 +24,54 @@ const Header = () => {
           />
         </Link>
         <div className="hidden md:flex items-center space-x-8">
-          <a href="#features" className="text-gray-600 hover:text-blue-600">
-            Features
-          </a>
-          <a href="#testimonials" className="text-gray-600 hover:text-blue-600">
-            Testimonials
-          </a>
+          <SignedOut>
+            <a href="#features" className="text-gray-600 hover:text-blue-600">
+              Features
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-600 hover:text-blue-600"
+            >
+              Testimonials
+            </a>
+          </SignedOut>
         </div>
         <div className="flex items-center space-x-4">
-          {/* <SignedIn> */}
-          {/* <Link
+          <SignedIn>
+            <Link
               href="/dashboard"
               className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
             >
-              <Button variant="outline">
-                <LayoutDashboard size={18} />
-                <span className="hidden md:inline">Dashboard</span>
-              </Button>
+              <button className="flex items-center justify-center gap-3 bg-white border hover:bg-gray-100 border-gray-200 text-black w-32 h-9 rounded-sm cursor-pointer">
+                <MdOutlineDashboard size={18} />
+                <span className="hidden md:inline text-sm">Dashboard</span>
+              </button>
             </Link>
             <a href="/transaction/create">
-              <Button className="flex items-center gap-2">
-                <PenBox size={18} />
-                <span className="hidden md:inline">Add Transaction</span>
-              </Button>
+              <button className="flex items-center justify-center gap-3 bg-black text-white hover:bg-gray-800 px-4 py-2 rounded-md cursor-pointer transition duration-200 ">
+                <GrTransaction size={18} />
+                <span className="hidden md:inline text-sm">
+                  Add Transaction
+                </span>
+              </button>
             </a>
-          </SignedIn> */}
-          {/* <SignedOut> */}
-          {/* <SignInButton forceRedirectUrl="/dashboard"> */}
-          <button className="text-black h-9 w-18 rounded-md hover:bg-gray-100  border border-gray-300 shadow-md cursor-pointer">
-            Login
-          </button>
-
-          {/* </SignInButton> */}
-          {/* </SignedOut> */}
-          {/* <SignedIn> */}
-          {/* <UserButton
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <button className="text-black h-9 w-18 rounded-md hover:bg-gray-100  border border-gray-300 shadow-md cursor-pointer">
+                Login
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
               appearance={{
                 elements: {
                   avatarBox: "w-10 h-10",
                 },
               }}
-            /> */}
-          {/* </SignedIn> */}
+            />
+          </SignedIn>
         </div>
       </nav>
     </header>
