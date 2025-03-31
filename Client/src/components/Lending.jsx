993e269b5194f34bd94f5bf37233d21b0b32192d
Nikhilg0208 <nikhilg1098@gmail.com>
2025-04-01 01:03:59 +0530

    change add

--- Client/src/components/Lending.jsx
+++ Client/src/components/Lending.jsx
@@ -34,7 +34,7 @@ const Lending = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {featuresData.map((feature, index) => (
               <Card
-                className="p-6 border shadow-sm"
+                className="p-6"
                 key={index}
               >
                 <CardContent className="space-y-4 pt-4 pb-4 w-6/7">
